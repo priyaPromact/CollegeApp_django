a0b0222343ea2daac4c6cdd540097f32d45a822c
priya <priya@promactinfo.com>
2015-09-22 11:22:03 +0530

    added manageApp models to admin site

--- collageapp/manageApp/admin.py
+++ collageapp/manageApp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import collegeAppUser, MainCategories, College, UserCollegeLink, UserCategoryLink, CollegeCategoryLink
 
 # Register your models here.
+admin.site.register(collegeAppUser)
+admin.site.register(MainCategories)
+admin.site.register(College)
+admin.site.register(UserCollegeLink)
+admin.site.register(UserCategoryLink)
+admin.site.register(CollegeCategoryLink)
